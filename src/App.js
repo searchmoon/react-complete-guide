c543222a2436bbd40ed87c2f6dfaa55dcaf22c81
searchmoon <moonding901212@gmail.com>
2022-02-24 11:53:31 +0900

    섹션3 마무리, 파일 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import logo from "./logo.svg";
 import "./App.css";
-import "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
