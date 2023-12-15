08b7b872da5f85795999c83db7df8d8c6bf71b23
Rumi <moonding901212@gmail.com>
2023-12-15 18:32:06 +0900

    컴포넌트 분리

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,11 +3,8 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesChart from "./ExpensesChart";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 
-/**
- *
- */
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
@@ -18,24 +15,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className={"expenses"}>
       <ExpensesFilter selected={filteredYear} onChangeHandler={filterChangeHandler} />
       <ExpensesChart expenses={filteredExpenses} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
